d833764a29fc172e998e6d460305fddfe6322de7
unknown <rubikonspace@ukr.net>
2023-11-14 19:31:23 +0200

    а

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -16,18 +16,18 @@ import {
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
-  };
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   dispatch(
+  //     register({
+  //       name: form.elements.name.value,
+  //       email: form.elements.email.value,
+  //       password: form.elements.password.value,
+  //     })
+  //   );
+  //   form.reset();
+  // };
 
   // return (
   //   <form onSubmit={handleSubmit} autoComplete="off">
