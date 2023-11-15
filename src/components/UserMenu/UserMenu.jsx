9e0e2d4811de09dc22ddbb7039d86280973dd3f8
unknown <rubikonspace@ukr.net>
2023-11-15 10:01:00 +0200

    redirect_login_logOut.Notify_toast

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,15 +2,26 @@ import { useAuth } from 'components/hooks';
 import { logOut } from 'components/redux/auth/operations';
 import { useDispatch } from 'react-redux';
 import { ConteinerWrapper, LogoutButton, UserName } from './UserMenu.styled';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { user } = useAuth();
 
   return (
     <ConteinerWrapper>
       <UserName>Welcome, {user.name}</UserName>
-      <LogoutButton type="button" onClick={() => dispatch(logOut())}>
+      <LogoutButton
+        type="button"
+        onClick={() =>
+          dispatch(logOut())
+            .unwrap()
+            .then(() => navigate('/'))
+            .catch(() => toast.error('UPS...'))
+        }
+      >
         Logout
       </LogoutButton>
     </ConteinerWrapper>
