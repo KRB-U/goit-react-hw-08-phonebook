1e8e340151a93d7be6506aaf17995f36515f4689
unknown <rubikonspace@ukr.net>
2023-11-16 21:06:00 +0200

    в

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -170,7 +170,7 @@ export default function RegisterForm() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Log in
+            Registration
           </Typography>
           <Box
             component="form"
@@ -215,7 +215,7 @@ export default function RegisterForm() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Log In
+              Register
             </Button>
           </Box>
         </Box>
